9637a879b5028aaf818416f88973b7b46c3144d9
hoainamnguyen123 <144362866+hoainamnguyen123@users.noreply.github.com>
2023-12-21 00:35:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ hands = mp_hand.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5
 )
-
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     success,img = cap.read()
@@ -17,12 +16,9 @@ while cap.isOpened():
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     result = hands.process(img)
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    count = 0
     label = ""
     if result.multi_hand_landmarks:
-
         myHand = []
-        count = 0
         for idx,hand in enumerate(result.multi_hand_landmarks):
 
             mp_drawing_util.draw_landmarks(img,hand,mp_hand.HAND_CONNECTIONS,
@@ -35,8 +31,14 @@ while cap.isOpened():
                 myHand.append([int(lm.x*w),int(lm.y*h)])
             if (myHand[8][1] < myHand[6][1] and myHand[20][1] < myHand[18][1]
             and myHand[12][1]> myHand[10][1] and myHand[16][1] > myHand[14][1]
-            and myHand[4][0] < myHand[2][0]):
+            and myHand[4][0] > myHand[2][0]):
                 label = "I LOVE YOU"
+            #một số xử lí khác....
+            if (myHand[8][1] > myHand[5][1] and myHand[20][1] > myHand[18][1]
+            and myHand[12][1] > myHand[10][1] and myHand[16][1] > myHand[14][1]
+            and myHand[4][0] < myHand[2][0] ):
+                label = "Nam dam"
+
             if(myHand[8][1] > myHand[5][1] and myHand[20][1] > myHand[18][1]
             and myHand[12][1] < myHand[10][1] and myHand[16][1] > myHand[14][1]
             and myHand[4][0] > myHand[2][0]):
@@ -51,24 +53,32 @@ while cap.isOpened():
                 label = "OKEY !!"
             #thatim
             #like
-            if (myHand[8][0] < myHand[5][0] and myHand[12][0] < myHand[9][0]
-            and myHand[16][0] < myHand[13][0] and myHand[20][0] < myHand[17][0]
+            if (myHand[8][0] < myHand[6][0] and myHand[12][0] < myHand[10][0]
+            and myHand[16][0] < myHand[14][0] and myHand[20][0] < myHand[18][0]
             and myHand[4][1] < myHand[2][1]):
                 label = " LIKE "
-
-            # arr =[8,12,16,20]
-            # for x in arr:
-            #     if (myHand[x][1] < myHand[x-2][1]):
-            #         count = count + 1
-            # if (myHand[5][0] < myHand[4][0]):
-            #     count = count + 1
-
-
-
+            #dislike
+            if (myHand[8][0] < myHand[6][0] and myHand[12][0] < myHand[10][0]
+            and myHand[16][0] < myHand[14][0] and myHand[20][0] < myHand[18][0]
+            and myHand[4][1] > myHand[2][1]):
+                label = "Dislike"
+            if (myHand[8][1] > myHand[5][1] and myHand[12][1] > myHand[9][1]
+            and myHand[16][1] > myHand[13][1] and myHand[20][1] > myHand[17][1]
+            and myHand[2][0] < myHand[4][0]):
+                label = "turn right"
+            if (myHand[8][1] > myHand[5][1] and myHand[12][1] > myHand[9][1]
+            and myHand[16][1] > myHand[13][1] and myHand[20][1] > myHand[17][1]
+            and myHand[4][0] < myHand[2][0] and not myHand[4][0] > myHand[13][0] and not myHand[4][1]>myHand[8][1]):
+                label = "turn left"
+            if (myHand[8][1] < myHand[5][1] and myHand[20][1] > myHand[18][1]
+                and myHand[12][1] > myHand[10][1] and myHand[16][1] > myHand[14][1]
+                and myHand[4][0] < myHand[2][0]):
+                label = "Forward"
+            if (myHand[8][1] > myHand[5][1] and myHand[20][1] < myHand[18][1] and myHand[12][1] < myHand[10][1] and
+                myHand[16][1] < myHand[14][1] and myHand[4][1] > myHand[2][1]):
+                label = "down"
 
     cv2.putText(img,str(label),(50,50),cv2.FONT_ITALIC,1,(255, 8, 90),2,cv2.LINE_AA)
-    cv2.rectangle(img,(550,55),(490,3),(225,0,0),-1)
-    cv2.putText(img, str(count), (500, 50), cv2.FONT_ITALIC, 2, (225,225,225), 2, cv2.LINE_AA)
     cv2.imshow("Hai ban tay cua em",img)
     key = cv2.waitKey(1)
     if key == 27:
